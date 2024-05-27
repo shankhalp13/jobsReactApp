e3207463a40ec7cdbc6b66b5fe1f6b28c483c597
shankhalp13 <shankhalppradhan2003@gmail.com>
2024-05-27 23:15:32 +0530

    if else used

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,23 @@ const App = () => {
   const name = 'Shankhalp';
   const x = 10; 
   const y = 20;
+  const names = ['Shankhalp','Sondeep','Amrit'];
+  const loggedIn = true;
+
+  if(loggedIn){
+    return <h1>Hello Member</h1>;
+  }
 
   return (
     <>
       <div className='text-5xl'>App</div>
       <p>Hello { name }</p>
       <p>The sum of {x} and {y} is {x+y}</p>
+      <ul>
+        {names.map((name ,index) => (
+          <li key ={ index } >{name}</li>
+        ))}
+      </ul>
     </>
     
   )
